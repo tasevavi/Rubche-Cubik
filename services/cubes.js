74f1ecb04c4740649448cd6e74eee0172476d9f6
Violeta T <taseva.vi@gmail.com>
2022-01-28 20:17:28 +0200

    clear old content from cube service

--- services/cubes.js
+++ services/cubes.js
@@ -1,16 +1,4 @@
-//const fs = require('fs').promises;
 const Cube = require('../models/Cube.js');
-
-// async function read() {
-//     try {
-//         const file = await fs.readFile('./services/data.json');
-//         return JSON.parse(file);
-//     } catch (err) {
-//         console.error('Database read error');
-//         console.error(err);
-//         process.exit(1)
-//     }
-// }
 
 function cubeViewModel(cube) {
     return {
@@ -21,16 +9,6 @@ function cubeViewModel(cube) {
         price: cube.price
     }
 }
-
-// async function write(data) {
-//     try {
-//         const file = await fs.writeFile('./services/data.json', JSON.stringify(data, null, 2));
-//     } catch (err) {
-//         console.error('Database read error');
-//         console.error(err);
-//         process.exit(1)
-//     }
-// }
 
 async function getAll(query) {
     const options = {};
@@ -50,24 +28,6 @@ async function getAll(query) {
 
     const cubes = await Cube.find(options); //or lean()
     return cubes.map(cubeViewModel);
-    // const data = await read();
-    // let cubes = Object
-    //     .entries(data)
-    //     .map(([id, v]) => Object.assign({}, {id}, v));
-
-    // if (query.search) {
-    //     cubes = cubes.filter(c => c.name.toLocaleLowerCase().includes(query.search.toLocaleLowerCase()));
-    // }
-
-    // if (query.from) {
-    //     cubes = cubes.filter(c => c.price >= Number(query.from));
-    // }
-
-    // if (query.to) {
-    //     cubes = cubes.filter(c => c.price <= Number(query.to));
-    // }
-
-    // return cubes;
 }
 
 async function getById(id) {
@@ -77,52 +37,20 @@ async function getById(id) {
     } else {
         return undefined;
     }
-    // const data = await read();
-    // const cube = data[id];
-
-    // if (cube) {
-    //     return Object.assign({}, {id}, cube);
-    // } else {
-    //     return undefined;
-    // }
 }
 
 async function createCube(cube) {
     const result = new Cube(cube);
     await result.save();
-    // const cubes = await read();
-    // let id = nextId();
-    // cubes[id] = cube;
-    // await write(cubes);
 }
 
 async function deleteById(id) {
     await Cube.findByIdAndDelete(id);
-    // const data = await read();
-    
-    // if (data.hasOwnProperty(id)) {
-    //     delete data[id];
-    //     await write(data);
-    // } else {
-    //     throw new ReferenceError('No such ID in database');
-    // }
 }
 
 async function updateById(id, cube) {
     await Cube.findByIdAndUpdate(id, cube);
-    // const data = await read();
-
-    // if (data.hasOwnProperty(id)) {
-    //     data[id] = cube;
-    //     await write(data);
-    // } else {
-    //     throw new ReferenceError('No such ID in database');
-    // }
 }
-
-// function nextId() {
-//     return 'xxxxxx-xxxx'.replace(/x/g, () => (Math.random()*16 | 0).toString(16));
-// }
 
 module.exports = () => (req, res, next) => {
     req.storage = {
