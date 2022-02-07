76a6c23a20f5a710faca7738d1153b13820f1c20
Violeta T <taseva.vi@gmail.com>
2022-02-07 12:55:27 +0200

    add user to session

--- controllers/home.js
+++ controllers/home.js
@@ -1,7 +1,7 @@
 module.exports = {
     async home(req, res) {
+        console.log('>>> Session:', req.session);
         const cubes = await req.storage.getAll(req.query);
-        //console.log('>>> cubes from storage.getAll', cubes);//undefined
         res.render('index', {cubes, title: 'Home', query: req.query});
     }
 };
--- services/authService.js
+++ services/authService.js
@@ -1,32 +1,40 @@
 const User = require('../models/User');
 
-async function register(username, password) {
+async function register(session, username, password) {
     const user = new User({
         username, 
         hashedPassword: password
     });
     await user.save();
+    session.user = {
+        id: user._id,
+        username: user.username
+    };
 }
 
 //this will login the user and redirect but wont put anything in the session
-async function login(username, password) {
+async function login(session, username, password) {
     const user = await User.findOne({username}); 
-    if (!user) {
-        throw new Error('Invalid username or password');
+
+    if (user && await user.comparePassword(password)) {
+        session.user = {
+            id: user._id,
+            username: user.username
+        };
+        return true;
     } else {
-        const result = await user.comparePassword(password);
-        if(result) {
-            return true;
-        } else {
-            throw new Error('Invalid username or password');
-        }
+        throw new Error('Invalid username or password');
     }
 }
 
 module.exports = () => (req, res, next) => {
+    if (req.session.user) {
+        res.locals.user = req.session.user;
+        res.locals.hasUser = true;
+    }
     req.auth = {
-        register,
-        login
+        register: (...params) => register (req.session, ...params),
+        login: (...params) => login (req.session, ...params)
     };
     next();
 };
